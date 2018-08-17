6ec627045dabe38861dfa5bb9497e745df25daf7
vbruneel <vincent.bruneel@decathlon.com>
2018-08-17 13:34:00 +0200

    modifs suite synchro

--- index.php
+++ index.php
@@ -87,8 +87,8 @@ $header = new Header($page);
 		</section>
 		<!-- Optional JavaScript -->
 		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-		<script src="js/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"></script>
-		<script src="js/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ"></script>
+		<script src="js/jquery-3.3.1.slim.min.js"></script>
+		<script src="js/popper.min.js" ></script>
 		<script src="js/bootstrap.min.js"></script>
 	</body>
 </html>
